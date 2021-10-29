0828f81c88b83d525a6c2a05cfeeda5e131be19a
JenniferJones216 <jenniferjones216@gmail.com>
2021-10-29 10:16:19 -0400

    backing up auto click

--- src/js/DonutShoppe.js
+++ src/js/DonutShoppe.js
@@ -6,17 +6,53 @@ class DonutShoppe {
         this.AutoClicksPerSecond = 0;
         this.MultiplierCount = 0;
         this.DonutsPerClick = 1;
-        this.MultiplierPrice = 100
+        this.MultiplierPrice = 100;
     }
 
     makeDonut(){
-        this.DonutCount++;
+        this.DonutCount += this.DonutsPerClick;
     }
 
-
-
+    makeHundredDonuts(){
+        this.DonutCount += 100;
+    }
 
     getDonutCount(){
-        return this.DonutCount;
+        return Math.round(this.DonutCount);
+    }
+
+    buyAutoClicker(){
+        this.AutoClickerCount += 1;
+        this.DonutCount -= this.AutoClickerPrice;
+        this.AutoClicksPerSecond += 1;
+        this.AutoClickerPrice *= 1.1; 
+    }
+
+       buyMultiplier(){
+        this.MultiplierCount += 1;
+    }
+
+    getAutoClickerCount(){
+        return this.AutoClickerCount;
+    }
+
+    getMultiplierCount(){
+        return this.MultiplierCount;
+    }
+
+    getAutoClickerPrice(){
+        return Math.round(this.AutoClickerPrice);
+    }
+
+    getMultiplierPrice(){
+        return Math.round(this.MultiplierPrice);
+    }
+
+    getAutoClicksPerSecond(){
+        return this.AutoClicksPerSecond;
+    }
+
+    getDonutsPerClick(){
+        return this.DonutsPerClick;
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,60 @@
-let shoppe = new DonutShoppe;
+const DonutCountDisplay = document.getElementById("DonutCountBox");
+const DonutMakerInput = document.getElementById("DonutMaker");
+const HundredDonutInput = document.getElementById("HundredDonutMaker");
+const AutoClickerCountDisplay = document.getElementById("AutoClickerCountBox");
+const BuyAutoClickerInput = document.getElementById("BuyAutoClickerButton");
+const AutoClickerPriceDisplay = document.getElementById("AutoClickerPriceBox");
+const ClicksPerSecondDisplay = document.getElementById("ClicksPerSecondBox");
+const MultiplierCountDisplay = document.getElementById("MultiplierCountBox");
+const BuyMultiplierInput = document.getElementById("BuyMultiplierButton");
+const MultiplierPriceDisplay = document.getElementById("MultiplierPriceBox");
+const DonutsPerClickDisplay = document.getElementById("DonutsPerClickBox");
 
-console.log(shoppe.getDonutCount());
+
+let donutshoppe = new DonutShoppe();
+
+// console.log(donutshoppe.getDonutCount());
+
+DonutCountDisplay.value = donutshoppe.getDonutCount();
+
+DonutMakerInput.addEventListener("click", function(){
+    donutshoppe.makeDonut();
+    DonutCountDisplay.value = donutshoppe.getDonutCount();})
+
+HundredDonutInput.addEventListener("click", function(){
+    donutshoppe.makeHundredDonuts();
+    DonutCountDisplay.value = donutshoppe.getDonutCount();})
+
+AutoClickerCountDisplay.value = donutshoppe.getAutoClickerCount();
+
+BuyAutoClickerInput.addEventListener("click", function(){
+    donutshoppe.buyAutoClicker();
+    DonutCountDisplay.value = donutshoppe.getDonutCount();
+    AutoClickerCountDisplay.value = donutshoppe.getAutoClickerCount();
+    AutoClickerPriceDisplay.value = donutshoppe.getAutoClickerPrice();
+    ClicksPerSecondDisplay.value = donutshoppe.getAutoClicksPerSecond();
+    let r = donutshoppe.getAutoClicksPerSecond();
+    let s = 1000/r;
+    setInterval(makeAutoClickDonut, s); 
+    
+    function makeAutoClickDonut() {
+        donutshoppe.makeDonut();
+        DonutCountDisplay.value = donutshoppe.getDonutCount();
+    }
+})
+
+
+AutoClickerPriceDisplay.value = donutshoppe.getAutoClickerPrice();
+
+ClicksPerSecondDisplay.value = donutshoppe.getAutoClicksPerSecond();
+
+MultiplierCountDisplay.value = donutshoppe.getMultiplierCount();
+
+
+
+MultiplierPriceDisplay.value = donutshoppe.getMultiplierPrice();
+
+
+
+DonutsPerClickDisplay.value = donutshoppe.getDonutsPerClick();
+
